d3b786ae204d5e86675b18fee7e256747d879511
kasimkkn <kasikkn15@gmail.com>
2024-02-26 17:13:12 +0530

    update

--- src/components/cart-item.tsx
+++ src/components/cart-item.tsx
@@ -17,11 +17,15 @@ const CartItem = ({
 }: CartItemProps) => {
   const { photo, productId, name, price, quantity } = cartItem;
 
-  return (
+  const words = name.split(" ");
+  const limitedName = words.slice(0, 6).join(" ");
+    return (
     <div className="cart-item">
+      <div className="cart-item-img">
       <img src={`${photo}`} alt={name} loading="lazy"/>
+      </div>
       <article>
-        <Link to={`/product/${productId}`}>{name}</Link>
+        <Link to={`/product/${productId}`}>{limitedName}</Link>
         <span>₹{price}</span>
       </article>
 
